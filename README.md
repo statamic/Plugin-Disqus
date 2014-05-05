c2c54f614f496b6f109ea800df12602a95ef2a8b
danielmerriott <daniel@merriott.net>
2014-05-06 00:23:16 +1200

    Added Comment Count Script functionality. Updated README.md
    Added countscript public function in pi.disqus.php.
    Added url variable to comment public function in pi.disqus.php.
    Bumped version number to 1.1 in pi.disqus.php.
    Updated README.md with details of changes.

--- README.md
+++ README.md
@@ -9,13 +9,30 @@ Disqus plugin for Statamic.
 2. Ensure the folder name is `disqus` (Github timestamps the download folder).
 3. Enjoy.
 
-## Usage
+## Usage - Comments
 
-Add Disqus comments to your page by dropping in the tag and setting the `account` parameter
+Add Disqus comments to your page by dropping in the `disqus:comments` tag and setting the `account` parameter
     
     {{ disqus:comments account="username" }}
 
-## Parameters
+## Parameters - Comments
 
 - `account`: your disqus account
-- `dev`: set to true/false to turn on developer mode
+- `dev`: _optional_ set to true/false to turn on developer mode
+- `url`: _optional_ set to “{url}” to use the relative path of the page as the URL to uniquely identify each page. Useful if you host your site with two URLS (e.g. for testing purposes) as comment counts and comments will work correctly. This parameter is passed to the `disqus_url` Disqus variable. See [Disqus Javascript Configuration Variables](http://help.disqus.com/customer/portal/articles/472098-javascript-configuration-variables#disqus_url) for more details.
+
+## Usage - Comment Count Script
+
+Add the Disqus comment count to your page by dropping in the tag`disqus:countscript` to your page (preferably at the end of the `<body>`).
+
+This will embed the Disqus script to replace the following markup:
+
+    <a href="http://example.com/article2.html#disqus_thread">Second article</a>
+
+with the comment count, for example:
+
+    <a href="http://example.com/article2.html#disqus_thread">13 Comments</a>
+
+If you used the `url` parameter above in your Disqus tag then you can use the attribute `data-disqus-identifier` in your anchor links and the href attribute can be whatever you like.
+
+See the Disqus help page [Adding comment count links to your home page](http://help.disqus.com/customer/portal/articles/565624-tightening-your-disqus-integration)
--- pi.disqus.php
+++ pi.disqus.php
@@ -3,12 +3,14 @@ class Plugin_disqus extends Plugin {
 
     var $meta = array(
         'name'       => 'Disqus',
-        'version'    => '1',
+        'version'    => '1.1',
     );
 
     public function comments()
     {
         $account        = $this->fetchParam('account', null);
+        $url            = $this->fetchParam('url', null);
+
         $developer_mode = $this->fetchParam('dev', false, false, true);
 
         $developer_mode = $developer_mode ? '1' : '0';
@@ -18,7 +20,14 @@ class Plugin_disqus extends Plugin {
         <div id="disqus_thread"></div>
         <script type="text/javascript">
             var disqus_shortname = "'.$account.'"; // required: replace example with your forum shortname
-            var disqus_developer = '.$developer_mode.';
+            var disqus_developer = '.$developer_mode.';'
+
+            if ($url) {
+                $output .= '
+        var disqus_identifier = "'.$url.'";';
+            }
+
+            output .= '
             /* * * DON\'T EDIT BELOW THIS LINE * * */
             (function() {
                 var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;
@@ -28,6 +37,29 @@ class Plugin_disqus extends Plugin {
         </script>
         <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
         <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+        ';
+            return $output;
+        }
+
+        return '';
+    }
+
+    public function countscript()
+    {
+        $account        = $this->fetchParam('account', null);
+
+        if ($account) {
+            $output = '
+        <script type="text/javascript">
+            var disqus_shortname = "'.$account.'"; // required: replace example with your forum shortname
+            /* * * DON\'T EDIT BELOW THIS LINE * * */
+            (function () {
+            var s = document.createElement("script"); s.async = true;
+            s.type = "text/javascript";
+            s.src = "http://" + disqus_shortname + ".disqus.com/count.js";
+            (document.getElementsByTagName("HEAD")[0] || document.getElementsByTagName("BODY")[0]).appendChild(s);
+            }());
+        </script>
         ';
             return $output;
         }
